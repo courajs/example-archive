105bd73918cc0f5f8011da0b720c15d93f3fc9fa
Aaron Sikes <jingadjanga@gmail.com>
2015-01-26 18:32:53 -0500

    Add main game logic of checking pairs, and flipping back over

--- js/app.js
+++ js/app.js
@@ -1,26 +1,100 @@
 (function(){
 
-var $root = $('#app')
-var c, rank, suit;
+App.el = $('#app')
+App.cards = []
 
-var cards = []
+var MEMORIZATION_TIME = 1250	// time a non-match stay flipped (ms)
 
-for (rank = 0; rank<13; rank++){
-	for (suit = 0; suit<4; suit++){
-		c = new App.Card(rank, suit);
-		$root.append(c.el)
-		cards.push(c)
+App.playerHasWon = function(){
+	return App.cards.every(function(card){
+		return card.flipped
+	})
+}
+
+App.newGame = function(){
+	var cards = App.cards = []
+	var c, rank, suit
+	for (rank = 0; rank<13; rank++){
+		for (suit = 0; suit<4; suit++){
+			c = new App.Card(rank, suit)
+			App.el.append(c.el)
+			cards.push(c)
+		}
+	}
+	App.el.on('click', '.card', mainHandler)
+}
+
+App.newGame()
+
+
+
+var flipped = undefined
+var frozen = false
+
+function freeze(){
+	frozen = true
+	App.el.addClass('frozen')
+}
+function unfreeze(){
+	frozen = false
+	App.el.removeClass('frozen')
+}
+
+function mainHandler(){
+	if (frozen){
+		console.log('nope, frozen')
+		return				// ignore input while a non-match are face-up for memorization (and after victory)
+	}
+
+	var card = App.cards[$(this).index()]
+
+	if (card.flipped){
+		console.log('nope, already flipped')
+		return		// either clicked it again after flipping it, or they've already matched it
+	}
+
+	card.flip()
+	if (!flipped) {		// first card of a pair - easy
+		console.log("flipping first card: "+card)
+		flipped = card
+	} else {	// second card - check for match etc.
+		console.log("flipping second card: "+card, "first card was: "+flipped)
+		freeze()
+		if (cardsMatch(flipped, card)) {
+			successfulMatch()
+		} else {
+			failedMatch(flipped, card)
+		}
 	}
 }
 
-$root.on('click', '.card', function(){
-	var card = cards[$(this).index()]
-	if (card.cleared) return;
-	
-	card.flip()
+function cardsMatch(card1, card2){
+	var match = card1.rank === card2.rank
+	console.log('match:', match)
+	return match
+}
+
+function failedMatch(card1, card2){
 	setTimeout(function(){
-		card.clear()
-	}, 750)
-})
+		unfreeze()
+		card1.flip()
+		card2.flip()
+		flipped = undefined
+	}, MEMORIZATION_TIME)
+}
+
+function successfulMatch(){
+	if (App.playerHasWon()){
+		victory()
+	}
+	else {
+		unfreeze()
+		flipped = undefined
+	}
+}
+
+function victory(){
+	alert("Congratulations, you did it! You concentrated hard, and you figured it out. I'm so proud of you.")
+}
 
 })()
--- js/card.js
+++ js/card.js
@@ -1,13 +1,14 @@
 (function(){
 
-var template = '<div class="card down"><div class="front"><div class="rank"></div><div class="suit"></div></div></div>';
-var ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+var template = '<div class="card down"><div class="front"><div class="rank"></div><div class="suit"></div></div></div>'
+var ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
 var suits = ['♠︎','♥︎','♣︎','♦︎']
 
 function Card(rank, suit){
-	this.rank = rank;
-	this.suit = suit;
-	this.render();
+	this.rank = rank
+	this.suit = suit
+	this.flipped = false
+	this.render()
 }
 
 Card.prototype.render = function(){
@@ -21,14 +22,14 @@ Card.prototype.render = function(){
 }
 
 Card.prototype.flip = function(){
+	this.flipped = !this.flipped
 	this.el.toggleClass('down')
 }
 
-Card.prototype.clear = function(){
-	this.cleared = true;
-	this.el.addClass('cleared')
+Card.prototype.toString = function(){
+	return ranks[this.rank] + ' of ' + suits[this.suit]
 }
 
-window.App.Card = Card;
+window.App.Card = Card
 
 })()
